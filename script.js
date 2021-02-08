a0e63b96d0b97980161bc11ee494ddc8179cc74f
Michael Buts <bumihail701@gmail.com>
2021-02-08 20:48:03 +0300

    built  practical lesson_12

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных филмов?',''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из поледних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
